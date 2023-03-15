c498e9da9a8a81aef8c420cebc82a9357660ce0d
Sebastian Kalciov <66016734+ducktheduck@users.noreply.github.com>
2023-03-15 17:00:37 +0200

    Update speech.py

--- src/scripts/speech.py
+++ src/scripts/speech.py
@@ -2,19 +2,18 @@
 
 import speech_recognition as sr
 
-# Declared the recognizer
 rec = sr.Recognizer()
 
 # Listening the audio
 with sr.Microphone() as source:
-    print('zi ceva uai ci cu tini')
+    print('Say something.')
     audio = rec.listen(source)
 
 # Trying to recognize the audio
 try:
     print('JTRON thinks you said: ', rec.recognize_sphinx(audio))
 except Exception as e:
-    print('da vorbeste cuaimiu clar:  {0}'.format(e))
+    print('Error:  {0}'.format(e))
 
 
 
